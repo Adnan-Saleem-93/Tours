bfd3410ee9d8d47a97966998c34b5b009eda962a
Adnan Saleem <adnan13893@gmail.com>
2021-06-22 11:08:51 +0500

    remove commented code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Button} from "react-bootstrap";
-// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-// import {faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 
 const Header = ({isData, refetchData}) => {
   const refreshHandler = () => {
@@ -10,12 +8,7 @@ const Header = ({isData, refetchData}) => {
 
   return (
     <div className="header">
-      <h3>
-        {isData ? "Our Tours" : "No Tours Left"}
-        {/* <Button variant="outline-danger" title="Clear All Tours">
-          <FontAwesomeIcon icon={faTrashAlt} />
-        </Button> */}
-      </h3>
+      <h3>{isData ? "Our Tours" : "No Tours Left"}</h3>
       {!isData && (
         <Button variant="primary" onClick={refreshHandler}>
           Refresh
